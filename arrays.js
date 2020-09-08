029b7d9d67ad0904601ee408f729f4d50faca269
nia-minumula <niam.ravenclaw@gmail.com>
2020-09-08 16:14:49 -0500

    Objects

--- arrays.js
+++ arrays.js
@@ -1,1 +1,18 @@
-console.log("Arrays")
+console.log("Arrays");
+const arr = ['Ann', 'Bob', 10, 20, 'Hello', [100,200,300]];
+console.log(arr);
+console.log(arr[5][1]);
+
+const colors = ['red', 'blue', 'green', 'white'];
+// add at the end
+colors.push('yellow');
+// remove at the end
+colors.pop();
+// remove from beginning
+colors.shift();
+// add at the beginning
+colors.unshift('teal')
+console.log(colors);
+
+//find index of item
+console.log(colors.indexOf('teal'));
---
+++ objects.js
@@ -0,0 +1,13 @@
+const person = {}
+person.firstname = 'Praveen';
+person['lastname'] = 'Minumula';
+
+person.kid = {
+    name: 'NM'
+}
+
+
+console.log(person);
+console.log(person.firstname);
+
+console.log(person.kid);
