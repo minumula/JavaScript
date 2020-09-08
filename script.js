90458ba5e1766e11b6aba2cf87e6903981640038
nia-minumula <niam.ravenclaw@gmail.com>
2020-09-08 14:43:36 -0500

    Added additional files

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+function passExam(name, score){
+    const pass = 71;
+    const fail = 35;
+
+    if (score >= 70) { 
+        console.log(name + 'has passed with ' + score);
+    }
+    else if (score <= 35) {
+        console.log(name + 'has failed with ' + score);
+    }
+}
+
+passExam('Praveen',105);
+*/
+
+// const multiply = function(x,y){
+//     const z = x * y;
+//     return z;
+// }
+
+
+// console.log (multiply(10,5));
+
+const multiply = (x,y) => x * y;
+    
+
+console.log (multiply(10,5));
